4ab1e51b2bf1162524e02e385a17700b0bec8dc4
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2024-02-11 00:00:51 -0800

    Removing cognito domain from output variables.

--- terraform/Cognito/output.tf
+++ terraform/Cognito/output.tf
@@ -2,10 +2,6 @@ output "COGNITO_USER_POOL_NAME" {
   value = aws_cognito_user_pool.polyglot.id
 }
 
-output "COGNITO_USER_POOL_DOMAIN" {
-  value = aws_cognito_user_pool_domain.cognito_domain
-}
-
 output "COGNITO_USER_POOL_CLIENT_NAME" {
     value = aws_cognito_user_pool_client.polyglot.id
 }
