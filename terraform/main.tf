dc1c8d7e224b20781bc57c09702a0a34862506f4
Snehal_Nikam <snehalvnikam@gmail.com>
2024-02-14 19:12:14 -0800

    Snehal | updated main - removed security and s3 module

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,21 +10,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
-#module "security" {
-#  source = "./security"
-#}
-#
-#module "S3" {
-#  source = "./s3"
-#}
-
-#module "cognito" {
-#  source = "./cognito"
-#  CALLBACK_URLS = ""
-#  COGNITO_USER_POOL_CLIENT_NAME = ""
-#  COGNITO_USER_POOL_NAME = ""
-#  LOGOUT_URLS = ""
-#}
+module "cognito" {
+  source = "./cognito"
+  CALLBACK_URLS = ""
+  COGNITO_USER_POOL_CLIENT_NAME = ""
+  COGNITO_USER_POOL_NAME = ""
+  LOGOUT_URLS = ""
+}
 
 module "lambda" {
   source = "./lambda"
