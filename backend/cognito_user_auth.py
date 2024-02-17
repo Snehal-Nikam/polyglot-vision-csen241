1568dea540ea780fe1ce06177abcaa0bb8615b61
Silvi Monga <mongasilvi@gmail.com>
2024-02-16 19:07:17 -0800

    added logic for  cognito user authentication functionality

---
+++ backend/cognito_user_auth.py
@@ -0,0 +1,14 @@
+import boto3
+
+
+cognito = boto3.client('cognito-idp', region_name='us-east-1')
+
+def is_valid_user(user_pool, user_id):
+    try:
+        cognito.admin_get_user(
+            UserPoolId=user_pool,
+            Username=user_id
+        )
+        return True
+    except cognito.exceptions.UserNotFoundException:
+        return False
--- backend/s3_helpers.py
+++ backend/s3_helpers.py
@@ -20,7 +20,6 @@ def upload_to_s3(user_id, file_name, file_stream, s3_bucket, prefix=None):
         # upload the file to s3
         s3_client = boto3.client('s3', region_name='us-east-1')
         s3_client.upload_fileobj(file_stream, s3_bucket, s3_key)
-        
         return file_id
     
     except s3_client.exceptions.ClientError as exc:
