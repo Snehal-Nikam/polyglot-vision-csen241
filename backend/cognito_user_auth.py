2a9a76309e1fcfce18205be92263271907e7560a
Silvi Monga <mongasilvi@gmail.com>
2024-02-17 23:49:07 -0800

    changed region to us-west-2 in cognito file and added logic for inetraction with dynamoDB

--- backend/cognito_user_auth.py
+++ backend/cognito_user_auth.py
@@ -1,7 +1,7 @@
 import boto3
 
 
-cognito = boto3.client('cognito-idp', region_name='us-east-1')
+cognito = boto3.client('cognito-idp', region_name='us-west-2')
 
 def is_valid_user(user_pool, user_id):
     try:
