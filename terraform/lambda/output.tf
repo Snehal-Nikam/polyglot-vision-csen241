bc07425cf474747606aacce7a6ab66b833f82df8
Snehal_Nikam <snehalvnikam@gmail.com>
2024-02-20 15:26:34 -0800

    Snehal | output file for lambda

--- terraform/lambda/output.tf
+++ terraform/lambda/output.tf
@@ -1,3 +1,11 @@
-output "transcribe-lambda-arn" {
+output "subtitles-lambda-arn" {
   value = aws_lambda_function.subtitles-lambda.arn
+}
+
+output "transcribe-lambda-arn" {
+  value = aws_lambda_function.transcribe-lambda.arn
+}
+
+output "translate-lambda-arn" {
+  value = aws_lambda_function.translate-lambda.arn
 }
