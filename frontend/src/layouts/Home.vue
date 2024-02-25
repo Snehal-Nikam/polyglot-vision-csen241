87986d5c69ca3f879abf9a9fbe8fa8f1a7b36a5d
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2024-02-24 18:05:51 -0800

    Fixing errors in frontend configuration.

--- frontend/src/layouts/Home.vue
+++ frontend/src/layouts/Home.vue
@@ -1,45 +1,87 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-        <q-toolbar-title>Welcome {{user.name}}</q-toolbar-title>
-        <q-btn flat dense round icon="logout" @click="signOut"/>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-      :width="250"
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <template v-for="(menuItem, index) in menuLinks">
-          <q-item
-            :key="index"
-            clickable
-            :active="index == activeIndex"
-            @click="setActive(index)"
-          >
-            <q-item-section avatar>
-              <q-icon :name="menuItem.icon" />
-            </q-item-section>
-            <q-item-section>{{ menuItem.label }}</q-item-section>
-          </q-item>
-          <q-separator :key="'sep' + index" v-if="menuItem.separator" />
-        </template>
-      </q-list>
-    </q-drawer>
-    <q-page-container>
-      <FileUploader v-if="activeIndex == 0" />
-    </q-page-container>
-  </q-layout>
-</template>
-
+    <q-layout view="lHh Lpr lFf">
+      <q-header elevated>
+        <q-toolbar>
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            @click="leftDrawerOpen = !leftDrawerOpen"
+          />
+          <q-toolbar-title>Welcome {{user.name}}</q-toolbar-title>
+          <q-btn flat dense round icon="logout" @click="signOut"/>
+        </q-toolbar>
+      </q-header>
+  
+      <q-drawer
+        v-model="leftDrawerOpen"
+        bordered
+        :width="250"
+        content-class="bg-grey-1"
+      >
+        <q-list>
+          <template v-for="(menuItem, index) in menuLinks">
+            <q-item
+              :key="index"
+              clickable
+              :active="index == activeIndex"
+              @click="setActive(index)"
+            >
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>{{ menuItem.label }}</q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+        </q-list>
+      </q-drawer>
+      <q-page-container>
+        <FileUploader v-if="activeIndex == 0" />
+      </q-page-container>
+    </q-layout>
+  </template>
+  
+  <script>
+  const menuList = [
+    {
+      icon: 'home',
+      label: 'Início'
+    }
+  ]
+  import FileUploader from '../function/FileUploader'
+  import authService from '../service/authService'
+  export default {
+    components: { FileUploader },
+    data () {
+      return {
+        leftDrawerOpen: false,
+        menuLinks: menuList,
+        activeIndex: 0,
+        user: {}
+      }
+    },
+    methods: {
+      setActive (idx) {
+        this.activeIndex = idx
+      },
+      async signOut () {
+        try {
+          await authService.signOut()
+          this.$router.push('/')
+        } catch (err) {
+          console.log(err)
+          this.$q.notify({
+            message: 'Error when exiting the application',
+            color: 'red'
+          })
+        }
+      }
+    },
+    async created () {
+      const { attributes: data } = await authService.getCurrentUser()
+      this.user = data
+    }
+  }
+  </script>
--- frontend/src/layouts/SignUp.vue
+++ frontend/src/layouts/SignUp.vue
@@ -1,54 +1,123 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <div class="container">
-      <q-card>
-        <img class="avatar" src="tubarao.jpg" />
-        <q-card-section>
-          <q-form @submit="Register" class="q-gutter-md q-pa-sm">
-            <q-input
-              v-model="user.name"
-              label="Your name"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || 'Blank field']"
-            />
-
-            <q-input
-              v-model="user.email"
-              label="Email"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || 'Blank field']"
-            />
-
-            <q-input
-              type="password"
-              v-model="user.password"
-              label="Password"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || 'Blank field']"
-            />
-
-            <q-input
-              type="password"
-              v-model="confpass"
-              label="Confirm Password"
-              lazy-rules
-              :rules="[
-                (val) => (val && val.length > 0) || 'Blank field',
-                (val) => val === user.password || 'Passwords do not match',
-              ]"
-            />
-
-            <div>
-              <q-btn
-                :loading="loading"
-                label="Register"
-                type="submit"
-                color="primary"
+    <q-layout view="lHh Lpr lFf">
+      <div class="container">
+        <q-card>
+          <img class="avatar" src="" />
+          <q-card-section>
+            <q-form @submit="Register" class="q-gutter-md q-pa-sm">
+              <q-input
+                v-model="user.name"
+                label="Your name"
+                lazy-rules
+                :rules="[(val) => (val && val.length > 0) || 'Blank field']"
               />
-            </div>
-          </q-form>
-        </q-card-section>
-      </q-card>
-    </div>
-  </q-layout>
-</template>
+  
+              <q-input
+                v-model="user.email"
+                label="Email"
+                lazy-rules
+                :rules="[(val) => (val && val.length > 0) || 'Blank field']"
+              />
+  
+              <q-input
+                type="password"
+                v-model="user.password"
+                label="Password"
+                lazy-rules
+                :rules="[(val) => (val && val.length > 0) || 'Blank field']"
+              />
+  
+              <q-input
+                type="password"
+                v-model="confpass"
+                label="Confirm Password"
+                lazy-rules
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Blank field',
+                  (val) => val === user.password || 'Passwords do not match',
+                ]"
+              />
+  
+              <div>
+                <q-btn
+                  :loading="loading"
+                  label="Register"
+                  type="submit"
+                  color="primary"
+                />
+              </div>
+            </q-form>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-layout>
+  </template>
+  
+  <script>
+  import authService from '../service/authService'
+  export default {
+    data () {
+      return {
+        user: {
+          name: '',
+          password: '',
+          email: ''
+        },
+        confpass: '',
+        loading: false
+      }
+    },
+    methods: {
+      async Register () {
+        const { email: username, password, name } = this.user
+        try {
+          this.loading = true
+          await authService.signUp({ username, password, name })
+          this.$router.push('/')
+        } catch (err) {
+          this.$q.notify({
+            message: 'Error processing data',
+            color: 'red'
+          })
+        } finally {
+          this.loading = false
+        }
+      }
+    }
+  }
+  </script>
+  <style lang="stylus" scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+  }
+  .q-card {
+    margin-top: 60px;
+    height: 580px;
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+  }
+  .q-icon {
+    margin: 5px;
+    margin-left: auto;
+    padding: 10px;
+    border-radius: 5px;
+    font-size: 15px;
+    cursor: pointer;
+    transition: all 0.2;
+  }
+  .q-icon:hover {
+    background-color: #aaa;
+  }
+  .avatar {
+    margin: 10px auto;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .q-btn {
+    width: 100%;
+    height: 40px;
+  }
+  </style>
