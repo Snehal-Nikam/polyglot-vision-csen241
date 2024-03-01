9680fed5fb9e60477446d08025d60be896adb7d2
Snehal_Nikam <snehalvnikam@gmail.com>
2024-03-01 13:56:34 -0800

    Snehal | subtitle api import issue

--- subtitle-api/ec2_user_data_amazonlinux.sh
+++ subtitle-api/ec2_user_data_amazonlinux.sh
@@ -2,12 +2,12 @@
 
 sudo yum update -y
 sudo yum install git -y
-git clone https://github.com/flych3r/cloud-computing-video-subtitles.git
-cd cloud-computing-video-subtitles/caption_api
+git clone https://github.com/Snehal-Nikam/polyglot-vision-csen241.git
+cd polyglot-vision-csen241/subtitle-api
 sudo bash install_ffmpeg_amazonlinux.sh
 sudo bash install_imagemagick_amazonlinux.sh
 sudo yum install python3 -y
 sudo python3 -m pip install -r requirements.txt
 export API_FLASK_HOST=0.0.0.0
-export API_FLASK_PORT=8080
+export API_FLASK_PORT=8082
 python3 main.py
