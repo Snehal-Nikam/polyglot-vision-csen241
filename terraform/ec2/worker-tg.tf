4487be3c38c985dead85cf6dedbb19b89e8d7db1
sourabhdeshmukh <sourabh.deshmukh.988@gmail.com>
2024-03-05 00:31:45 -0800

    Added ALB, TG, ASG and Launch Template terraform code.

---
+++ terraform/ec2/worker-tg.tf
@@ -0,0 +1,34 @@
+
+resource "aws_lb_target_group" "worker-tg" {
+    name                              = "worker-tg"
+    deregistration_delay              = "30"
+    ip_address_type                   = "ipv4"
+    load_balancing_algorithm_type     = "round_robin"
+    load_balancing_anomaly_mitigation = "off"
+    load_balancing_cross_zone_enabled = "use_load_balancer_configuration"
+    port                              = 80
+    protocol                          = "HTTP"
+    protocol_version                  = "HTTP1"
+    slow_start                        = 0
+    target_type                       = "instance"
+    vpc_id                            = "vpc-0fab6d89be4a1f3ae"
+
+    health_check {
+        enabled             = true
+        healthy_threshold   = 5
+        interval            = 30
+        matcher             = "200"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 2
+    }
+
+    stickiness {
+        cookie_duration = 86400
+        enabled         = false
+        type            = "lb_cookie"
+    }
+
+}
